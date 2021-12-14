b831b9777fc8c0bd9b97b017d99c0855e8edc35c
RenyyyK <reny_kulcsar@yahoo.com>
2021-12-14 16:24:12 +0200

    return to last errorless state

--- remi_kitchen/lib/authentication/auth_screen.dart
+++ remi_kitchen/lib/authentication/auth_screen.dart
@@ -1,7 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 import 'package:remi_kitchen/providers/auth.dart';
 
 enum AuthMode { Signup, Login }
@@ -68,7 +66,7 @@ class _AuthCardState extends State<AuthCard> {
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
-  void _submit() {
+  Future<void> _submit() async {
     if (!_formKey.currentState!.validate()) {
       // Invalid!
       return;
@@ -81,7 +79,7 @@ class _AuthCardState extends State<AuthCard> {
       // Log user in
     } else {
       // Sign user up
-      Provider.of<Auth>(context).signup(_authData['email'], _authData['password']);
+      // await Provider.of<Auth>(context, listen: false).signup(_authData['email'], _authData['password']);
     }
     setState(() {
       _isLoading = false;
--- remi_kitchen/lib/providers/auth.dart
+++ remi_kitchen/lib/providers/auth.dart
@@ -1,25 +1,25 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:flutter/widgets.dart';
-import 'package:http/http.dart' as http;
+// import 'package:flutter/widgets.dart';
+// import 'package:http/http.dart' as http;
 
-class Auth with ChangeNotifier {
-  late String token;
-  late DateTime expiryDate;
-  late String userId;
+// class Auth with ChangeNotifier {
+//   late String token;
+//   late DateTime expiryDate;
+//   late String userId;
 
-  Future<void> signup(String email, String password) async {
-    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDaaM3yioT6kjRJg60hTNwyUFd7jAtQWyc');
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true
-        },
-      ),
-    );
-    print(json.decode(response.body));
-  }
-}
+//   Future<void> signup(String? email, String? password) async {
+//     final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDaaM3yioT6kjRJg60hTNwyUFd7jAtQWyc');
+//     final response = await http.post(
+//       url,
+//       body: json.encode(
+//         {
+//           'email': email,
+//           'password': password,
+//           'returnSecureToken': true
+//         },
+//       ),
+//     );
+//     print(json.decode(response.body));
+//   }
+// }
